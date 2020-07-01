b339e3521cd9d22038fb8648b018a490287d1ae2
Jorge <jorsema2@gmail.com>
2020-07-01 10:46:52 +0200

    blsbls

--- src/index.js
+++ src/index.js
@@ -15,6 +15,11 @@ function TodoApp() {
 
   const [value, setValue] = useState('')
 
+  const [input, setInput] = useState({
+    value: '',
+    hidden: true
+  });
+
   function handleForm(e) {
     e.preventDefault();
     setTasks(tasks.concat({name: value, delete: false}));
@@ -25,16 +30,39 @@ function TodoApp() {
     setValue(e.target.value);
   }
 
-  function deleteTask(index) {
-    const newTasks = [...tasks];
-    newTasks.splice(index, 1);
+  function updateTask(task) {
+    console.log('hey');
+    if (el => el.name === task.name) {
+      console.log('hey');
+      return (
+        <div>
+          <p>Hey</p>
+        </div>
+      )
+    }
+  }
+
+  function deleteTask(task) {
+    const newTasks = tasks.filter(el => el.name !== task.name);
     setTasks(newTasks);
   }
 
-  const taskList = tasks.map((task, index) =>
-  <div key = {tasks.name}>
-    <li>{tasks[index].name}</li>
-    <button onClick={() => deleteTask(index)}>Delete</button>
+  function createSomething(e) {
+    e.preventDefault();
+    input.hidden = !input.hidden;
+    if (input.hidden === false) {
+      return <div><p>Hey</p></div>;
+    };
+  }
+
+  const taskList = tasks.map((task) =>
+  <div key = {task.name}>
+    <li>{task.name}</li>
+    <button onClick={() => updateTask(task)}>Update</button>
+    <button onClick={() => deleteTask(task)}>Delete</button>
+    <form onSubmit={createSomething}>
+      <button>Create</button>
+    </form>
   </div>
   )
 
@@ -50,3 +78,5 @@ function TodoApp() {
 }
 
 ReactDOM.render(<TodoApp />, document.getElementById("root"));
+
+// Link with the right question: https://www.google.com/search?q=show+input+box+when+clicked+react&oq=show+input+box+when+clicked+react&aqs=chrome..69i57j0.12244j0j7&sourceid=chrome&ie=UTF-8
