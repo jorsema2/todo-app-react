d7eca88cb5de8fb1f341408a6450a731e526d379
Cristian Florea <florecra@novartis.com>
2020-07-01 10:38:31 +0200

    mentoring

--- src/index.js
+++ src/index.js
@@ -1,42 +1,54 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
-
 
 function TodoApp() {
   const [tasks, setTasks] = useState([
     {
-      name: 'Fix bike',
-      delete: false
-    }, {
-      name: 'Clean room',
-      delete: false
-    }
+      name: "Fix bike",
+      completed: true,
+    },
+    {
+      name: "Clean room",
+      completed: false,
+    },
   ]);
 
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState("");
 
   function handleForm(e) {
     e.preventDefault();
-    setTasks(tasks.concat({name: value, delete: false}));
-    setValue('');
+    const newTasks = [...tasks, {name: value, completed: false}]
+    setTasks(newTasks);
+    setValue("");
   }
 
   function handleChange(e) {
     setValue(e.target.value);
   }
 
-  function deleteTask(index) {
-    const newTasks = [...tasks];
-    newTasks.splice(index, 1);
+  function deleteTask(task) {
+    setTasks(tasks.filter(el => el.name !== task.name));
+  }
+
+  function completedTask(task){
+    const newTasks = tasks.map(el => {
+      if(el.name === task.name){
+        el.completed = !el.completed;
+      }
+
+      return task;
+    });
+
     setTasks(newTasks);
   }
 
-  const taskList = tasks.map((task, index) =>
-  <div key = {tasks.name}>
-    <li>{tasks[index].name}</li>
-    <button onClick={() => deleteTask(index)}>Delete</button>
-  </div>
-  )
+  const taskList = tasks.map((task) => (
+    <div key={task.name} style={{ background: task.completed ? 'pink': 'white' }} >
+      <li>{task.name}</li>
+       <button onClick={() => completedTask(task)}>Complete</button>
+      <button onClick={() => deleteTask(task)}>Delete</button>
+    </div>
+  ));
 
   return (
     <div>
@@ -48,5 +60,35 @@ function TodoApp() {
     </div>
   );
 }
+/**
+ * 
+ * function useState(value){
+    let initialValue = value;
+    function changeValue(newValue){
+        initialValue = newValue;
+    }
+
+    return [initialValue, changeValue];
+}
+
+       data     update data         DB   initial value
+const [tasks,   setTasks       ] = useState([])
+
+
+
+
+class Todo extends React.Component {
+    state ={
+        todos: [], 
+        value: ''
+    }
+
+    deleteTodos(){
+        this.setState({ todos: [1,2,3,4], value: 'dog' })
+    }
+
+
+}
+ */
 
 ReactDOM.render(<TodoApp />, document.getElementById("root"));
