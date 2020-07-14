eb08f9f01d074595e605e51689279d56c1124135
Jorge <jorsema2@gmail.com>
2020-07-14 13:40:07 +0200

    Bin feature added

--- src/index.js
+++ src/index.js
@@ -1,74 +1,37 @@
 import React, {useState, useRef, useEffect} from "react";
 import ReactDOM from "react-dom";
 
-const TaskItem = (props) => {
-  const myInput = useRef(null);
-  const [editMode, setEditMode] = useState(false);
-  const [value, setValue] = useState(props.task.name);
-
-  useEffect(() => {
-    console.log("I have been changed!", editMode);
-    if (editMode) {
-      myInput.current.focus();
-    }
-  }, [editMode]);
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
-
-  const handleBlur = () => {
-    setEditMode(!editMode);
-    props.updateTasks({...props.task, name: value});
-  };
-
-  return (
-    <div>
-      <div
-        key={props.task.name}
-        style={{background: props.task.completed ? "pink" : "white"}}
-      >
-        {!editMode && <li onClick={() => setEditMode(!editMode)}>{value}</li>}
-        {editMode && (
-          <input
-            onChange={handleChange}
-            ref={myInput}
-            value={value}
-            onBlur={handleBlur}
-          />
-        )}
-        <button onClick={() => props.completedTask(props.task)}>
-          Complete
-        </button>
-        <button onClick={() => props.deleteTask(props.task)}>Delete</button>
-      </div>
-    </div>
-  );
-};
-
 function TodoApp() {
   const [tasks, setTasks] = useState([
     {
       name: "Fix bike",
       completed: true,
       update: false,
+      priority: 3,
       id: `${Math.random()}-${Math.random()}`,
     },
     {
       name: "Clean room",
       completed: false,
       update: false,
+      priority: null,
       id: `${Math.random()}-${Math.random()}`,
     },
   ]);
 
   const [value, setValue] = useState("");
+  
+  // Stores deleted tasks:
+  const [deletedTasks, setDeletedTasks] = useState([])
+
+  // When true, the recycle bin is open. Otherwise, it remains closed:
+  const [binState, setBinState] = useState(false);
 
   function handleForm(e) {
     e.preventDefault();
     const newTasks = [
       ...tasks,
-      {name: value, completed: false, id: `${Math.random()}-${Math.random()}`},
+      {name: value, completed: false, update: false, priority: null, id: `${Math.random()}-${Math.random()}`},
     ];
     setTasks(newTasks);
     setValue("");
@@ -76,10 +39,17 @@ function TodoApp() {
 
   function handleChange(e) {
     setValue(e.target.value);
+    console.log(tasks)
   }
 
   function deleteTask(task) {
-    setTasks(tasks.filter((el) => el.id !== task.id));
+    const elminatedTasks = [
+      ...deletedTasks, 
+      task
+    ];
+    setDeletedTasks(elminatedTasks);
+    const remainingTasks = tasks.filter((el) => el.id !== task.id);
+    setTasks(remainingTasks);
   }
 
   function completedTask(task) {
@@ -93,7 +63,6 @@ function TodoApp() {
   }
 
   const updateTasks = (task) => {
-    console.log(task);
     const newTasks = tasks.map((el) => {
       if (el.id === task.id) {
         return task;
@@ -103,6 +72,107 @@ function TodoApp() {
 
     setTasks(newTasks);
   };
+
+  // Changes task's priority:
+  function increasePriority(task) {
+    const newTasks = tasks.map((el) => {
+      if (el.id === task.id) {
+
+        if (task.priority !== 5) {
+          task.priority++;
+          console.log(task);
+        } else if (task.priority === null) {
+          task.priority = 1;
+        } else {
+          task.priority = null;
+        }
+      }
+      return el;
+    });
+
+    setTasks(newTasks);
+
+  }
+
+  // Recovers a task from the recycle bin:
+  function recoverTasks(deletedTask) {
+    setTasks([...tasks, deletedTask]);
+    setDeletedTasks(deletedTasks.filter((el) => el.id !== deletedTask.id));
+  }
+
+  // Deletes a task from the recycle bin:
+  function deleteTaskForever(deletedTask) {
+    setDeletedTasks(deletedTasks.filter((el) => el.id !== deletedTask.id));
+  }
+
+  // Recovers all tasks from the recycle bin:
+  function recoverAll() {
+    const allTasks = tasks.concat(deletedTasks);
+    setTasks(allTasks);
+    setDeletedTasks([]);
+  }
+
+  // Deletes all tasks from the recycle bin:
+  function deleteAllTasks() {
+    setDeletedTasks([]);
+  }
+
+  const TaskItem = (props) => {
+    const myInput = useRef(null);
+    const [editMode, setEditMode] = useState(false);
+    const [value, setValue] = useState(props.task.name);
+  
+    useEffect(() => {
+      if (editMode) {
+        myInput.current.focus();
+      }
+    }, [editMode]);
+  
+    const handleChange = (e) => {
+      setValue(e.target.value);
+
+    };
+  
+    const handleBlur = () => {
+      setEditMode(!editMode);
+      props.updateTasks({...props.task, name: value});
+    };
+  
+    return (
+      <div>
+        <div
+          key={props.task.name}
+          style={{background: props.task.completed ? "pink" : "white"}}
+        >
+          {!editMode && <li onClick={() => setEditMode(!editMode)}>{value}</li>}
+          {editMode && (
+            <input
+              onChange={handleChange}
+              ref={myInput}
+              value={value}
+              onBlur={handleBlur}
+            />
+          )}
+          {props.task.priority && 
+          <button onClick={() => increasePriority(props.task)}>Priority {props.task.priority}</button>}
+          {!props.task.priority && 
+          <button onClick={() => increasePriority(props.task)}>No priority</button>}
+          <button onClick={() => props.completedTask(props.task)}>
+            Complete
+          </button>
+          <button onClick={() => props.deleteTask(props.task)}>Delete</button>
+        </div>
+      </div>
+    );
+  };
+
+  const recycleBin = deletedTasks.map((deletedTask) => (
+      <ul>
+        <li style={{background: deletedTask.completed ? "pink" : "white"}}>{deletedTask.name}</li>
+        <button onClick={() => recoverTasks(deletedTask)}>Recover</button>
+        <button onClick={() => deleteTaskForever(deletedTask)}>DeleteForever</button>
+      </ul>
+  ));
 
   const taskList = tasks.map((task) => (
     <TaskItem
@@ -120,159 +190,19 @@ function TodoApp() {
         <input onChange={handleChange} type="text" value={value}></input>
         <button>Add task to list</button>
       </form>
+      <button onClick={() => setBinState(!binState)}>RecycleBin</button>
+      {binState === true && 
+      <div>
+        <div>
+          {recycleBin}
+        </div>
+        <div>
+          <button onClick={() => recoverAll()}>Recover all tasks</button>
+          <button onClick={() => deleteAllTasks()}>Delete all tasks forever</button>
+        </div>
+      </div>}
     </div>
   );
 }
 
 ReactDOM.render(<TodoApp />, document.getElementById("root"));
-
-/* Code lost during econd Cristian's review
-
-import React, {useState} from "react";
-import ReactDOM from "react-dom";
-
-function TodoApp() {
-  const [tasks, setTasks] = useState([
-    {
-      name: "Fix bike",
-      completed: true,
-      update: false
-    },
-    {
-      name: "Clean room",
-      completed: false,
-      update: false
-    },
-  ]);
-
-  const [value, setValue] = useState("");
-
-  function handleForm(e) {
-    e.preventDefault();
-    const newTasks = [...tasks, {name: value, completed: false}]
-    setTasks(newTasks);
-    setValue("");
-  }
-
-  function handleChange(e) {
-    setValue(e.target.value);
-  }
-
-  function deleteTask(task) {
-    setTasks(tasks.filter(el => el.name !== task.name));
-  }
-
-  function completedTask(task){
-
-    const newTasks = tasks.map(el => {
-
-      if(el.name === task.name){
-        el.completed = !el.completed;
-
-      }
-      return el;
-    });
-    // Ask Cristian later through a loom video:
-    // console.log(newTasks);
-    setTasks(newTasks);
-    // console.log(tasks);
-  }
-
-  function updateTask(task) {
-
-    const newTasks = tasks.map(el => {
-
-      if(el.name === task.name){
-        el.update = !el.update;
-      }
-      return el;
-    });
-
-    setTasks(newTasks);
-  }
-
-  function NewInput() {
-    const [input, setInput] = useState("");
-
-    function updateInput(e) {
-      e.preventDefault();
-      const renameTasks = tasks.map(el => {
-        if(el.update === true) {
-          el.name = input;
-          el.update = false;
-        }
-        return el;
-      });
-      setTasks(renameTasks);
-    }
-  
-    function handleInput(e) {
-      console.log(input);
-      setInput(e.target.value);
-    }
-    
-    return(
-      <div>
-        <form onSubmit={updateInput}>
-          <input onChange={handleInput} type="text" value={input}></input>
-          <button>Update task</button>
-        </form>
-      </div>
-    )
-  }
-
-  const taskList = tasks.map((task) => (
-    <div>
-      <div key={task.name} style={{ background: task.completed ? 'pink': 'white' }} >
-        <li>{task.name}</li>
-        <button onClick={() => updateTask(task)}>Update</button>
-        <button onClick={() => completedTask(task)}>Complete</button>
-        <button onClick={() => deleteTask(task)}>Delete</button>
-        {task.update === true &&
-          <NewInput />
-        }
-      </div>      
-    </div>
-  ));
-  
-  return (
-    <div>
-      <ul>{taskList}</ul>
-      <form onSubmit={handleForm}>
-        <input onChange={handleChange} type="text" value={value}></input>
-        <button>Add task to list</button>
-      </form>
-    </div>
-  );
-}
-*/
-/* Code lost during first Cristian's review
-
-function useState(value){
-    let initialValue = value;
-    function changeValue(newValue){
-        initialValue = newValue;
-    }
-
-    return [initialValue, changeValue];
-}
-
-       data     update data         DB   initial value
-const [tasks,   setTasks       ] = useState([])
-
-
-
-
-class Todo extends React.Component {
-    state ={
-        todos: [], 
-        value: ''
-    }
-
-    deleteTodos(){
-        this.setState({ todos: [1,2,3,4], value: 'dog' })
-    }
-
-
-}
- */
